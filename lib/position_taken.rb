e5ed389bcc17a64f3afebbcee05f04f2675e6c6b
Otha D Hernandez <otha.hernandez@gmail.com>
2019-04-07 23:15:49 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,6 +1,7 @@
 # code your #position_taken? method here!
 
-def position_taken(position)
+def position_taken(board, index)
+  position = board[index]
   position == " " || "" || "X" || "O"
   
 end
